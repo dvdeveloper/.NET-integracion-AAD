b25dfd7621d20d471bff8b8e5c8fa5e1a15407c1
Diego Valladares <diego.valladares@usach.cl>
2021-12-22 12:15:46 -0300

    Proyecto de ejemplo

---
+++ AzureActiveDirectoryEJemplo/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AzureActiveDirectoryEJemplo.Controllers
+{
+    [System.Web.Mvc.Authorize]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //https://docs.microsoft.com/en-us/azure/active-directory/develop/tutorial-v2-asp-webapp
+            var userClaims = User.Identity as System.Security.Claims.ClaimsIdentity;
+            //You get the user's first and last name below:
+            ViewBag.Name = userClaims?.FindFirst("name")?.Value;
+            // The 'preferred_username' claim can be used for showing the username
+            ViewBag.Username = userClaims?.FindFirst("preferred_username")?.Value;
+            // The subject/ NameIdentifier claim can be used to uniquely identify the user across the web
+            ViewBag.Subject = userClaims?.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value;
+            // TenantId is the unique Tenant Id - which represents an organization in Azure AD
+            ViewBag.TenantId = userClaims?.FindFirst("http://schemas.microsoft.com/identity/claims/tenantid")?.Value;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
